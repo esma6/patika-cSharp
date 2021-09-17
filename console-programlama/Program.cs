a3115fbcda0277b7d823e29bed9d71e869464c8f
esma6 <77541320+esma6@users.noreply.github.com>
2021-09-17 16:42:37 +0300

    Add project files.

---
+++ console-programlama/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

f2630081f863282cd4a95b0e4795a3743e43ec7c
esma6 <77541320+esma6@users.noreply.github.com>
2021-09-18 14:17:09 +0300

    commit-6

---
+++ switchCase/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace switchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("Ocak");
+                    break;
+                case 2:
+                    Console.WriteLine("Şubat");
+                    break;
+                case 3:
+                    Console.WriteLine("Mart");
+                    break;
+                case 4:
+                    Console.WriteLine("Nisan");
+                    break;
+                case 5:
+                    Console.WriteLine("Mayıs");
+                    break;
+                case 6:
+                    Console.WriteLine("Haziran");
+                    break;
+                case 7:
+                    Console.WriteLine("Temmuz");
+                    break;
+                case 8:
+                    Console.WriteLine("Ağustos");
+                    break;
+                case 9:
+                    Console.WriteLine("Eylül");
+                    break;
+                case 10:
+                    Console.WriteLine("Ekim");
+                    break;
+                case 11:
+                    Console.WriteLine("Kasım");
+                    break;
+                case 12:
+                    Console.WriteLine("Aralık");
+                    break;
+                default:
+                    Console.WriteLine("Yanlış veri girişi!");
+                    break;
+            }
+
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kış");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("İlkbahar");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Yaz");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Sonbahar");
+                    break;
+                default:
+                    Console.WriteLine("Yanlış veri girişi!");
+                    break;
+            }
+        }
+    }
+}

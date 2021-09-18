8dfe8bf3064168434d0cea887483da8e06001f39
esma6 <77541320+esma6@users.noreply.github.com>
2021-09-18 14:56:48 +0300

    commit-8

---
+++ Diziler/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Diziler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayiDizisi = new int[20];
+            int toplam = 0;
+            for (int i = 0; i < sayiDizisi.Length; i++)
+            {
+                Console.Write("Lütfen {0}. sayıyı girin:", i + 1);
+                sayiDizisi[i] = Convert.ToInt32(Console.ReadLine());
+                toplam += sayiDizisi[i];
+            }
+            double ortalama = toplam / 20;
+            Console.WriteLine("Sayıların ortalaması: " + ortalama);
+            Console.ReadLine();
+        }
+    }
+}

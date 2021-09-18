1a1221afe5a32ae1d6b16e715ab4219d91eccc07
esma6 <77541320+esma6@users.noreply.github.com>
2021-09-18 14:05:38 +0300

    commit-4

---
+++ tryCatch/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace tryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Bir Sayı Giriniz: ");
+                int sayi1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Sayı: " + sayi1);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hata: " + ex.Message.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("İşlem Tamamlandı!");
+            }
+
+
+        }
+    }
+}

9426061f282c7699635770975995d4649ef16901
esma6 <77541320+esma6@users.noreply.github.com>
2021-09-21 12:58:47 +0300

    dorduncu soru

---
+++ odev1/dorduncuSoru.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace odev1
+{
+    //ODEV1 DORDUNCU SORU
+    //Bir konsol uygulamasında kullanıcıdan bir cümle yazması isteyin.
+    //Cümledeki toplam kelime ve harf sayısını console'a yazdırın.
+
+    class dorduncuSoru
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir cümle giriniz .");
+            string cumle = Console.ReadLine();
+            string liste = "ABCÇDEFGĞHİIJKLMNOÖPRSŞTUÜVYZ"
+                      + "abcçdefgğhiıjklmnoöprsştuüvyz";
+            string[] kelimeler = cumle.Split(' ');
+            Console.WriteLine("Kelime Sayısı :" + kelimeler.Length);
+
+            int sayac = 0;
+            for (int i = 0; i < cumle.Length; i++)
+            {
+                //karakter katarkter kontrol ediyoruz.
+                if (liste.Contains(cumle[i]))
+                {
+                    sayac++;
+                }
+            }
+            Console.WriteLine("toplam {0} tane  harf vardır.", sayac);
+            Console.ReadLine();
+        }
+    }
+}
--- odev1/ucuncusoru.cs
+++ odev1/ucuncusoru.cs
@@ -10,24 +10,24 @@ namespace odev1
         //Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin(n).
         //Sonrasında kullanıcıdan n adet kelime girmesi isteyin.
         //Kullanıcının girişini yaptığı kelimeleri sondan başa doğru console'a yazdırın.
-        static void Main(string[] args)
-        {
-            List<string> kelimeler = new List<string>();
-            Console.Write("Pozitif bir sayi giriniz ");
-            int n = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("{0} adet kelime giriniz", n);
-            for (int i = 0; i < n; i++)
-            {
-                Console.WriteLine("{0}.Kelimeyi Giriniz;", i + 1);
-                string kelime = Console.ReadLine();
-                kelimeler.Add(kelime);
-            }
-            int son = kelimeler.Count - 1;
-            Console.WriteLine("---sondan başa doğru kelimeler---");
-            for (int i = son; i >= 0; --i)
-            {
-                Console.WriteLine(kelimeler[i]);
-            }
-        }
+        //static void Main(string[] args)
+        //{
+        //    List<string> kelimeler = new List<string>();
+        //    Console.Write("Pozitif bir sayi giriniz ");
+        //    int n = Convert.ToInt32(Console.ReadLine());
+        //    Console.WriteLine("{0} adet kelime giriniz", n);
+        //    for (int i = 0; i < n; i++)
+        //    {
+        //        Console.WriteLine("{0}.Kelimeyi Giriniz;", i + 1);
+        //        string kelime = Console.ReadLine();
+        //        kelimeler.Add(kelime);
+        //    }
+        //    int son = kelimeler.Count - 1;
+        //    Console.WriteLine("---sondan başa doğru kelimeler---");
+        //    for (int i = son; i >= 0; --i)
+        //    {
+        //        Console.WriteLine(kelimeler[i]);
+        //    }
+        //}
     }
 }

27bc4119d0d830910e98a9553873c8f244839723
esma6 <77541320+esma6@users.noreply.github.com>
2021-09-21 11:20:29 +0300

    commit-9

---
+++ extensionMetot/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace extensionMetot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            bool isEven = MyExtensionClass.IsEventNumber(123);
+            Console.WriteLine(isEven);
+
+            int sonuc=MyExtensionClass.FibonacciSerisi(9);
+            Console.WriteLine(sonuc);
+
+        }
+    }
+    public static class MyExtensionClass
+    {
+        //extansion
+        public static bool IsEventNumber(this int value)
+        {
+            if (value % 2 == 0)
+                return true;
+            else
+                return false;
+        }
+
+        //rekürsif
+        public static int FibonacciSerisi(int sayi)
+        {
+            if (sayi == 0)
+            {
+                return 0;
+            }
+
+            else if (sayi == 1)
+            {
+                return 1;
+            }
+
+            else
+            {
+                return FibonacciSerisi(sayi - 1) + FibonacciSerisi(sayi - 2);
+            }
+
+        }
+
+    }
+
+
+}

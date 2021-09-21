8bb2225ee0ff4dbe0c15b1ce7615c1f456bebcbe
esma6 <77541320+esma6@users.noreply.github.com>
2021-09-21 11:56:36 +0300

    odev1 1.soru

---
+++ odev1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace odev1
+{ 
+    //   ODEV1 1.SORU
+    //Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin(n). 
+    //Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin.
+    //Kullanıcının girmiş olduğu sayılardan çift olanlar console'a yazdırın.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Pozitif bir sayi giriniz ");
+            int n= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0} adet sayi giriniz",n);
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("{0}.Sayıyı Giriniz;", i+1);
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                if (sayi % 2 == 0)
+                {
+                    Console.WriteLine("Çift "+sayi);
+                }
+            }
+
+        }
+    }
+}

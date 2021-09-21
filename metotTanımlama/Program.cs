51abfcea957eb509f7a3eb2401288658f170a1a0
esma6 <77541320+esma6@users.noreply.github.com>
2021-09-21 10:44:07 +0300

    .

---
+++ metotTanımlama/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace metotTanımlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //erişim_belirteci geri_donus_tipi metot_adi(argüman / parametre) 
+            //{
+            //    //komutlar;
+            //}
+
+            //ref
+            int x = 3;
+            int y = 4;
+            int sonuc = ArttırVeTopla(ref x, ref y);
+            Console.WriteLine(sonuc);
+
+            int ArttırVeTopla(ref int x, ref int y)
+            {
+                x += 1;
+                y += y;
+                return (x + y);
+            }
+            
+        }
+    }
+}

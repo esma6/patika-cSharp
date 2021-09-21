2f1e353b9c95e6be1d57489e7ce13c332f860715
esma6 <77541320+esma6@users.noreply.github.com>
2021-09-21 12:36:02 +0300

    ucuncusoru

--- odev1/ikinciSoru.cs
+++ odev1/ikinciSoru.cs
@@ -10,22 +10,22 @@ namespace odev1
     //Kullanıcının girmiş olduğu sayılardan m'e eşit yada tam bölünenleri console'a yazdırın.
     class ikinciSoru
     {
-        static void Main(string[] args)
-        {
-            Console.Write("Pozitif iki sayi giriniz ");
-            int n = Convert.ToInt32(Console.ReadLine());
-            int m = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("{0} adet sayi giriniz", n);
-            for (int i = 0; i < n; i++)
-            {
-                Console.WriteLine("{0}.Sayıyı Giriniz;", i + 1);
-                int sayi = Convert.ToInt32(Console.ReadLine());
-                if ((sayi == m) || (sayi % m == 0))
-                {
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("Pozitif iki sayi giriniz ");
+        //    int n = Convert.ToInt32(Console.ReadLine());
+        //    int m = Convert.ToInt32(Console.ReadLine());
+        //    Console.WriteLine("{0} adet sayi giriniz", n);
+        //    for (int i = 0; i < n; i++)
+        //    {
+        //        Console.WriteLine("{0}.Sayıyı Giriniz;", i + 1);
+        //        int sayi = Convert.ToInt32(Console.ReadLine());
+        //        if ((sayi == m) || (sayi % m == 0))
+        //        {
 
-                    Console.WriteLine("Tam bölünüyor " + sayi);
-                }
-            }
-        }
+        //            Console.WriteLine("Tam bölünüyor " + sayi);
+        //        }
+        //    }
+        //}
     }
 }
---
+++ odev1/ucuncusoru.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace odev1
+{
+    class ucuncusoru
+    {
+        //odev1 ucuncu soru
+        //Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin(n).
+        //Sonrasında kullanıcıdan n adet kelime girmesi isteyin.
+        //Kullanıcının girişini yaptığı kelimeleri sondan başa doğru console'a yazdırın.
+        static void Main(string[] args)
+        {
+            List<string> kelimeler = new List<string>();
+            Console.Write("Pozitif bir sayi giriniz ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0} adet kelime giriniz", n);
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("{0}.Kelimeyi Giriniz;", i + 1);
+                string kelime = Console.ReadLine();
+                kelimeler.Add(kelime);
+            }
+            int son = kelimeler.Count - 1;
+            Console.WriteLine("---sondan başa doğru kelimeler---");
+            for (int i = son; i >= 0; --i)
+            {
+                Console.WriteLine(kelimeler[i]);
+            }
+        }
+    }
+}
